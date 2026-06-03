c62337db5102df739b7c214e712358653b209563
JohanWilliamSanchez <35948452+JohanWilliamSanchez@users.noreply.github.com>
2026-06-03 03:57:54 +0000

    Crear mi libreria compartida

---
+++ vars/pipelineLambdaNode.groovy
@@ -0,0 +1,57 @@
+// vars/pipelineEstandar.groovy
+def call(Map config = [:]) {
+    pipeline {
+        agent any
+
+        environment {
+            APP_NAME = "${config.appName ?: 'AppSinNombre'}"
+        }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    echo "📥 [Checkout] Descargando código de: ${env.APP_NAME}"
+                    checkout scm
+                }
+            }
+
+            stage('Build') {
+                // Evaluamos si el parámetro 'runBuild' es true (por defecto si no viene, se ejecuta)
+                when {
+                    expression { return config.runBuild != false }
+                }
+                steps {
+                    echo "🏗️ [Build] Compilando la aplicación..."
+                }
+            }
+
+            stage('Test') {
+                // Si la app pasa 'runTest: false', este stage se omitirá por completo
+                when {
+                    expression { return config.runTest != false }
+                }
+                steps {
+                    echo "🧪 [Test] Ejecutando pruebas..."
+                }
+            }
+
+            stage('Security Scan') {
+                when {
+                    expression { return config.runSecurity != false }
+                }
+                steps {
+                    echo "🛡️ [Security Scan] Escaneando vulnerabilidades..."
+                }
+            }
+
+            stage('Deploy') {
+                when {
+                    expression { return config.runDeploy != false }
+                }
+                steps {
+                    echo "🚀 [Deploy] Desplegando..."
+                }
+            }
+        }
+    }
+}
